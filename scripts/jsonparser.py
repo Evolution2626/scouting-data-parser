282b6f2f95f17f3c84f35abf27e6f33cb37eecf1
Félix Giffard <technonerdzfg@gmail.com>
2023-03-28 11:23:32 -0400

    Remove duplicates in json data

--- scripts/jsonparser.py
+++ scripts/jsonparser.py
@@ -13,12 +13,15 @@ output_data = []
 
 for file_name in os.listdir(directory_path):
     if "json" in file_name:
-        with open(os.path.join(directory_path, file_name), "r", encoding='utf-8-sig') as file:
+        with open(os.path.join(directory_path, file_name), "r") as file:
             file_data = json.load(file)
             if isinstance(file_data, list):
                 output_data.extend(file_data)
             else:
                 output_data.append(file_data)
+
+# Remove duplicates
+output_data = list(dict.fromkeys(output_data))
 
 with open(output_file_path, "w") as output_file:
     json.dump(output_data, output_file)
